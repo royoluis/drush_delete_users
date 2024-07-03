457752426bc0136a5f6a0844316a2fb4b8d1ea7b
Luis Royo Antín <luisroyozgz@gmail.com>
2024-07-04 00:54:00 +0200

    Create files for BatchServices and custom commands

---
+++ src/BatchServices.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\drush_delete_users;
+
+/**
+ * Batch services for deleting users by role.
+ */
+class BatchServices {
+
+	public static function deleteUsers($id, $user, &$context) {
+		$user->delete();
+
+		$context['results'][] = $user->id();
+		$context['message'] = t('Finishing batch @id from user @uid.', [
+			'@id' => $id,
+			'@uid' => $user->id(),
+		]);
+	}
+
+	public static function deleteUserFinished($success, array $results) {
+		$messenger = \Drupal::messenger();
+		if($success) {
+			$messenger->addMessage(t('@count users deleted.', ['@count' => count($results)]));
+		}
+		else {
+			$messenger->addMessage(t('Error in the user deletion.'));
+		}
+	}
+
+}
---
+++ src/Commands/DeleteUsersByRoleCommand.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Drupal\drush_delete_users\Commands;
+
+use Drush\Commands\DrushCommands;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Deletes all users with a specified role.
+ *
+ * @package Drupal\drush_delete_users\Command
+ */
+class DeleteUsersByRoleCommand extends DrushCommands {
+
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new DeleteUsersByRoleCommand object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $string_translation
+   *   The string translation service.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * Deletes all users with a specified role.
+   *
+   * @param string $role
+   *   The role to delete users for.
+   *
+   * @command drush_delete_users:delete-users-by-role
+   * @aliases cudur
+   * @param string $role The role to delete users for.
+   * @usage drush_delete_users:delete-users-by-role editor
+   *   Deletes all users with the role 'editor'.
+   */
+  public function deleteUsersByRole($role) {
+    $operations = [];
+		$num_operations = 0;
+		$batch_id = 1;
+    
+    $query = $this->entityTypeManager->getStorage('user')->getQuery();
+    $uids = $query->condition('status', 1)
+                 ->condition('roles', $role)
+                 ->accessCheck(FALSE)
+                 ->execute();
+
+    if(!empty($uids)) {
+      $users = $this->entityTypeManager->getStorage('user')->loadMultiple($uids);
+      foreach ($users as $user) {
+        $operations[] = [
+          '\Drupal\drush_delete_users\BatchServices::deleteUsers',
+          [
+            $batch_id,
+            $user
+            ],
+          ];
+          $num_operations++;
+          $batch_id++;        
+      }
+    }
+    else {
+      $this->output()->writeln(sprintf('No users found with the role "%s"', $role));
+    }
+  
+		$batch = [
+			'title' => t('Comprobando @num items', ['@num' => $num_operations]),
+			'operations' => $operations,
+			'finished' => '\Drupal\drush_delete_users\BatchServices::deleteUserFinished',
+		];
+
+		batch_set($batch);
+		drush_backend_batch_process();
+		$this->output()->writeln("Process completed.");
+  }
+
+}

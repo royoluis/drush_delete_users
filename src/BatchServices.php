8f3a7706f5de954f24ce9c4509cd927e37dbe58b
Luis Royo Antín <luisroyozgz@gmail.com>
2025-03-07 18:45:25 +0100

    Fix identation and add comments

--- src/BatchServices.php
+++ src/BatchServices.php
@@ -7,23 +7,29 @@ namespace Drupal\drush_delete_users;
  */
 class BatchServices {
 
-	public static function deleteUsers($id, $user, &$context) {
-		$user->delete();
-		$context['results'][] = $user->id();
-		$context['message'] = t('Finishing batch @id from user @uid.', [
-			'@id' => $id,
-			'@uid' => $user->id(),
-		]);
-	}
+  /**
+   * {@inheritdoc}
+   */
+  public static function deleteUsers($id, $user, &$context) {
+    $user->delete();
+    $context['results'][] = $user->id();
+    $context['message'] = t('Finishing batch @id from user @uid.', [
+      '@id' => $id,
+      '@uid' => $user->id(),
+    ]);
+  }
 
-	public static function deleteUserFinished($success, array $results) {
-		$messenger = \Drupal::messenger();
-		if($success) {
-			$messenger->addMessage(t('@count users deleted.', ['@count' => count($results)]));
-		}
-		else {
-			$messenger->addMessage(t('Error in the user deletion.'));
-		}
-	}
+  /**
+   * {@inheritdoc}
+   */
+  public static function deleteUserFinished($success, array $results) {
+    $messenger = \Drupal::messenger();
+    if($success) {
+      $messenger->addMessage(t('@count users deleted.', ['@count' => count($results)]));
+    }
+    else {
+      $messenger->addMessage(t('Error in the user deletion.'));
+    }
+  }
 
 }
